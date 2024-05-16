7e18271a1e4bffd958163e966f82fa0259bb711a
Xuejing Huang <149098311+hxjwillbegood@users.noreply.github.com>
2024-05-16 21:39:27 +0800

    lab3

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import { HashRouter, Route, Routes, Navigate} from "react-router-dom";
-import logo from './logo.svg';
-import Labs from './Labs/Lab3';
-
+import Labs from "./Labs";
+import Kanbas from "./Kanbas";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 function App() {
   return (
     <HashRouter>
-    <div>
-      <Routes>
-        <Route path="/" element={<Navigate to="Labs" />} />
-        <Route path="/Labs/*" element={<Labs />} />
-      </Routes>
-    </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
     </HashRouter>
   );
 }
-
 export default App;
