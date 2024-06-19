a88f42db59e61259611f78ae36ed1e781f4939f1
Xuejing Huang <149098311+hxjwillbegood@users.noreply.github.com>
2024-06-19 22:31:04 +0800

    forth time

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -22,7 +22,7 @@ export default function Kanbas() {
               <Route path="Courses/:cid/*" element={<Courses />} />
               <Route path="Calendar" element={<h1>Calendar</h1>} />
               <Route path="Inbox" element={<h1>Inbox</h1>} />
-              <Route path="Courses/:cid/Assignments/:asmt" element={<AssignmentEditor />} />
+              <Route path="Courses/:cid/Assignments/:asmt/*" element={<AssignmentEditor />} />
             </Routes>
           </div>
         </div>
