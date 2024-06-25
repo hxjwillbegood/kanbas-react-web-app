dbc08daef8b7f409afb510ff8293bb292d6c2077
Xuejing Huang <149098311+hxjwillbegood@users.noreply.github.com>
2024-06-25 21:59:50 +0800

    a3 JS

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,18 @@
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent"
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <div id="wd-passing-functions">
+      <h2>Lab 4</h2>
+      <ClickEvent/>
+      <PassingDataOnEvent/>
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject/>
+    </div>
+  );
+}
