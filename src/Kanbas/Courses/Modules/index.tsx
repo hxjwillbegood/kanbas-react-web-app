ffd445bb433aaa9877fe15c9046bb4683593f817
Xuejing Huang <149098311+hxjwillbegood@users.noreply.github.com>
2024-07-03 15:14:49 +0800

    a4 Redux

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,41 +1,75 @@
-import ModulesControls from "./ModulesControls";
-import { BsGripVertical } from "react-icons/bs";
-import LessonControlButtons from "./LessonControlButtons"; 
-import ModuleControlButtons from "./ModuleControlButtons";
+import React, { useState } from "react";
 import { useParams } from "react-router";
-import * as db from "../../Database";
-
+import { useSelector, useDispatch } from "react-redux";
+import { BsGripVertical } from "react-icons/bs";
+import ModulesControls from "./ModulesControls";
+import ModuleControlButtons from "./ModuleControlButtons";
+import LessonControlButtons from "./LessonControlButtons";
+import { addModule, editModule, updateModule, deleteModule } from "./reducer";
 
 export default function Modules() {
   const { cid } = useParams();
-  const modules = db.modules;
-    return (
-      <div id="wd-modules">
-        <ModulesControls /><br /><br /><br /><br />
-        <ul id="wd-modules" className="list-group rounded-0">
+  const [moduleName, setModuleName] = useState("");
+  const modules = useSelector((state: any) => state.modulesReducer.modules);
+  const dispatch = useDispatch();
+
+  return (
+    <div id="wd-modules">
+      <ModulesControls
+        moduleName={moduleName}
+        setModuleName={setModuleName}
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }}
+      />
+      <br /><br /><br /><br />
+      <ul id="wd-modules" className="list-group rounded-0">
         {modules
           .filter((module: any) => module.course === cid)
           .map((module: any) => (
-          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-            <div className="wd-title p-3 ps-2 bg-secondary">
-              <BsGripVertical className="me-2 fs-3" />
-              {module.name}
-              <ModuleControlButtons />  
-            </div>
-            {module.lessons && (
-            <ul className="wd-lessons list-group rounded-0">
-              {module.lessons.map((lesson: any) => (
-              <li className="wd-lesson list-group-item p-3 ps-1" style={{borderLeft: '4px solid green'}}>
+            <li key={module._id} className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+              <div className="wd-title p-3 ps-2 bg-secondary">
                 <BsGripVertical className="me-2 fs-3" />
-                {lesson.name}
-                <LessonControlButtons />
-              </li>
-              ))}
-            </ul>
-            )}
-          </li>
+                {!module.editing && module.name}
+                {module.editing && (
+                  <input
+                    className="form-control w-50 d-inline-block"
+                    onChange={(e) =>
+                      dispatch(
+                        updateModule({ ...module, name: e.target.value })
+                      )
+                    }
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        dispatch(updateModule({ ...module, editing: false }));
+                      }
+                    }}
+                    value={module.name}
+                  />
+                )}
+                <ModuleControlButtons
+                  moduleId={module._id}
+                  deleteModule={() => {
+                    dispatch(deleteModule(module._id));
+                  }}
+                  editModule={() => dispatch(editModule(module._id))}
+                />
+              </div>
+              {module.lessons && (
+                <ul className="wd-lessons list-group rounded-0">
+                  {module.lessons.map((lesson: any) => (
+                    <li key={lesson._id} className="wd-lesson list-group-item p-3 ps-1" style={{ borderLeft: '4px solid green' }}>
+                      <BsGripVertical className="me-2 fs-3" />
+                      {lesson.name}
+                      <LessonControlButtons />
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
           ))}
-        </ul>
-      </div>
-
-  );}
+      </ul>
+    </div>
+  );
+}
--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -2,6 +2,18 @@ import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent"
 import PassingFunctions from "./PassingFunctions";
 import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
+import HelloRedux from "./ReduxExamples/HelloRedux";
+import CounterRedux from "./ReduxExamples/CounterRedux";
+import AddRedux from "./ReduxExamples/AddRedux";
+import TodoList from "./ReduxExamples/todos/TodoList";
 export default function Lab4() {
   function sayHello() {
     alert("Hello");
@@ -13,6 +25,18 @@ export default function Lab4() {
       <PassingDataOnEvent/>
       <PassingFunctions theFunction={sayHello} />
       <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>
+      <ReduxExamples/>
+      <HelloRedux/>
+      <CounterRedux/>
+      <AddRedux/>
+      <TodoList/>
     </div>
   );
 }
